3d5c3b0b3361bedae80c35abc897fb6f79279569
f0ghua <fog.hua@outlook.com>
2017-06-24 14:58:15 +0800

    + add can test tool

---
+++ canTestTool/canTestTool.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-06-24T11:07:37
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = canTestTool
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    ctt.qrc
---
+++ canTestTool/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDesktopWidget>
+#include <QFont>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    const QRect availableGeometry = QApplication::desktop()->availableGeometry(this);
+    QFont font;
+    int fontId = QFontDatabase::addApplicationFont(":/fonts/LucidaTypewriterRegular.ttf");
+    if (fontId != -1) {
+        const QStringList families = QFontDatabase::applicationFontFamilies(fontId);
+        if (!families.empty()) {
+            font.setFamily(families.at(0));
+            font.setPointSize(8);
+        }
+    } else {
+        //font.setFamily(QStringLiteral("Courier"));
+        font.setPointSize(9);
+    }
+
+    qApp->setFont(font);
+
+    int h = availableGeometry.height() * 3 / 4;
+    int w = h * 850 / 600;
+    resize(w, h);
+    setIconSize(QSize(16, 16));
+    return;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
